62665e6e97efccc864a4843c43b10325ba2fffaa
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-09-28 23:47:09 +0700

    Config delect

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -1,86 +1,35 @@
 <?php
+
 namespace Config;
+
 use CodeIgniter\Database\Config;
-/**
- * Database Configuration
- */
+
 class Database extends Config
 {
-    /**
-     * The directory that holds the Migrations
-     * and Seeds directories.
-     */
-    public string $filesPath = APPPATH . 'Database' . DIRECTORY_SEPARATOR;
-
-    /**
-     * Lets you choose which connection group to
-     * use if no other is specified.
-     */
-    public string $defaultGroup = 'default';
-
-    /**
-     * The default database connection.
-     *
-     * @var array<string, mixed>
-     */
-
-public array $default = [
-    'DSN'          => '',
-        'hostname' => env('database.default.hostname'),
-        'username' => env('database.default.username'),
-        'password' => env('database.default.password'),
-        'database' => env('database.default.database'),
-        'DBDriver' => env('database.default.DBDriver'),
-        'port'     => env('database.default.port'),
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8',
-        'DBCollat'     => 'utf8_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'numberNative' => false,
-];
-    /**
-     * This database connection is used when
-     * running PHPUnit database tests.
-     *
-     * @var array<string, mixed>
-     */
-    public array $tests = [
-        'DSN'         => '',
-        'hostname'    => '127.0.0.1',
-        'username'    => '',
-        'password'    => '',
-        'database'    => ':memory:',
-        'DBDriver'    => 'SQLite3',
-        'DBPrefix'    => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
-        'pConnect'    => false,
-        'DBDebug'     => true,
-        'charset'     => 'utf8',
-        'DBCollat'    => 'utf8_general_ci',
-        'swapPre'     => '',
-        'encrypt'     => false,
-        'compress'    => false,
-        'strictOn'    => false,
-        'failover'    => [],
-        'port'        => 3306,
-        'foreignKeys' => true,
-        'busyTimeout' => 1000,
-    ];
+    public array $default;
 
     public function __construct()
     {
         parent::__construct();
 
-        // Ensure that we always set the database group to 'tests' if
-        // we are currently running an automated test suite, so that
-        // we don't overwrite live data on accident.
-        if (ENVIRONMENT === 'testing') {
-            $this->defaultGroup = 'tests';
-        }
+        $this->default = [
+            'DSN'      => '',
+             'hostname' => env('database.default.hostname'),
+        'username' => env('database.default.username'),
+        'password' => env('database.default.password'),
+        'database' => env('database.default.database'),
+        'DBDriver' => env('database.default.DBDriver'),
+        'port'     => env('database.default.port'),
+            'DBPrefix' => '',
+            'pConnect' => false,
+            'DBDebug'  => (ENVIRONMENT !== 'production'),
+            'charset'  => 'utf8',
+            'DBCollat' => 'utf8_general_ci',
+            'swapPre'  => '',
+            'encrypt'  => false,
+            'compress' => false,
+            'strictOn' => false,
+            'failover' => []
+        ];
     }
 }

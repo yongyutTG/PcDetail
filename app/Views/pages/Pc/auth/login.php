a55c6b77d5bfde668b71f878a378e8af19235b57
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-09-28 21:46:33 +0700

    เปลี่ยนไอคอนและข้อวคามหน้า login

--- app/Views/pages/Pc/auth/login.php
+++ app/Views/pages/Pc/auth/login.php
@@ -8,7 +8,8 @@
       <div class="col-md-5 col-lg-4">
         <div class="card shadow-lg">
           <div class="card-body p-4">
-            <h4 class="text-center mb-4"><i class="bi bi-person-circle"></i>ระบบ PC Detail</h4>
+
+            <h4 class="text-center mb-4"><i class="bi bi-laptop"></i>ระบบเครื่องคอมพิวเตอร์</h4>
 
             <form id="loginForm" method="post">
 

df03366cddb3f99850e5a4747e94a509f3f13ddf
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-09-28 23:34:48 +0700

    edit

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,12 +29,12 @@ class Database extends Config
 
 public array $default = [
     'DSN'          => '',
-      'hostname' => env('database.default.hostname'),
-    'username' => env('database.default.username'),
-    'password' => env('database.default.password'),
-    'database' => env('database.default.database'),
-    'DBDriver' => env('database.default.DBDriver'),
-    'port'     => env('database.default.port'),
+        'hostname' => env('database.default.hostname'),
+        'username' => env('database.default.username'),
+        'password' => env('database.default.password'),
+        'database' => env('database.default.database'),
+        'DBDriver' => env('database.default.DBDriver'),
+        'port'     => env('database.default.port'),
         'DBPrefix'     => '',
         'pConnect'     => false,
         'DBDebug'      => true,

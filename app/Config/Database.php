1a9e164772ef072092f9617fbdab2399e7dcfd3a
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-09-30 00:17:57 +0700

    edit

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -36,15 +36,15 @@ public $default = [
 
 ];
 
-    // public function __construct()
-    // {
-    //     parent::__construct();
+    public function __construct()
+    {
+        parent::__construct();
 
-    //     // Ensure that we always set the database group to 'tests' if
-    //     // we are currently running an automated test suite, so that
-    //     // we don't overwrite live data on accident.
-    //     if (ENVIRONMENT === 'testing') {
-    //         $this->defaultGroup = 'tests';
-    //     }
-    // }
+        // Ensure that we always set the database group to 'tests' if
+        // we are currently running an automated test suite, so that
+        // we don't overwrite live data on accident.
+        if (ENVIRONMENT === 'testing') {
+            $this->defaultGroup = 'tests';
+        }
+    }
 }

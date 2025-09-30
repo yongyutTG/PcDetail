315ab753d5b92c1d7ca02d185407c676b515a25e
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-09-30 19:35:53 +0700

    edit

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -9,74 +9,42 @@ use CodeIgniter\Database\Config;
  */
 class Database extends Config
 {
-    /**
-     * The directory that holds the Migrations
-     * and Seeds directories.
-     */
-    public string $filesPath = APPPATH . 'Database' . DIRECTORY_SEPARATOR;
+ 
+public $filesPath = APPPATH . 'Database' . DIRECTORY_SEPARATOR;
+public $defaultGroup = 'default';
 
-    /**
-     * Lets you choose which connection group to
-     * use if no other is specified.
-     */
-    public string $defaultGroup = 'default';
 
-    /**
-     * The default database connection.
-     *
-     * @var array<string, mixed>
-     */
-public array $default = [
-    'DSN'          => '',
-        'hostname' => env('database.default.hostname'),
-        'username' => env('database.default.username'),
-        'password' => env('database.default.password'),
-        'database' => env('database.default.database'),
-        'DBDriver' => env('database.default.DBDriver'),
-        'port'     => env('database.default.port'),
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8',
-        'DBCollat'     => 'utf8_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'numberNative' => false,
+public $default = [
+    'DSN'      => '',
+    'hostname' => '',
+    'username' => '',
+    'password' => '',
+    'database' => '',
+    'DBDriver' => '',
+    'port'     => '',
+    'DBPrefix' => '',
+    'pConnect' => false,
+    'DBDebug'  => true,
+    'charset'  => 'utf8',
+    'DBCollat' => 'utf8_general_ci',
+    'swapPre'  => '',
+    'encrypt'  => false,
+    'compress' => false,
+    'strictOn' => false,
+    'failover' => [],
+    'numberNative' => false,
 ];
-    /**
-     * This database connection is used when
-     * running PHPUnit database tests.
-     *
-     * @var array<string, mixed>
-     */
-    public array $tests = [
-        'DSN'         => '',
-        'hostname'    => '127.0.0.1',
-        'username'    => '',
-        'password'    => '',
-        'database'    => ':memory:',
-        'DBDriver'    => 'SQLite3',
-        'DBPrefix'    => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
-        'pConnect'    => false,
-        'DBDebug'     => true,
-        'charset'     => 'utf8',
-        'DBCollat'    => 'utf8_general_ci',
-        'swapPre'     => '',
-        'encrypt'     => false,
-        'compress'    => false,
-        'strictOn'    => false,
-        'failover'    => [],
-        'port'        => 3306,
-        'foreignKeys' => true,
-        'busyTimeout' => 1000,
-    ];
 
     public function __construct()
     {
         parent::__construct();
+
+        $this->default['hostname'] = env('database.default.hostname');
+        $this->default['username'] = env('database.default.username');
+        $this->default['password'] = env('database.default.password');
+        $this->default['database'] = env('database.default.database');
+        $this->default['DBDriver'] = env('database.default.DBDriver');
+        $this->default['port']     = env('database.default.port');
 
         // Ensure that we always set the database group to 'tests' if
         // we are currently running an automated test suite, so that

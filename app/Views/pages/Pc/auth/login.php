a651cc8c2b7c71fb0bb0f397e6cea862a5591009
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-10-02 09:37:57 +0700

    edit login

--- app/Views/pages/Pc/auth/login.php
+++ app/Views/pages/Pc/auth/login.php
@@ -36,39 +36,12 @@
               <button type="submit" id="loginBtn" class="btn btn-login w-100">
                 <i class="bi bi-box-arrow-in-right"></i> เข้าสู่ระบบ
               </button>
-              <!-- ปุ่มลืมรหัสผ่าน -->
-<!-- <div class="d-flex justify-content-between mt-2">
-    <button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#forgotPasswordModal">
-        ลืมรหัสผ่าน?
-    </button>
-</div> -->
 
           </div>
         </div>
       </div>
     </div>
   </div>
-
-  <!-- Modal -->
-<!-- <div class="modal fade" id="forgotPasswordModal" tabindex="-1" aria-labelledby="forgotPasswordModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="forgotPasswordModalLabel">ลืมรหัสผ่าน</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form id="forgotPasswordForm">
-          <div class="mb-3">
-            <label for="emailInput" class="form-label">กรุณาใส่อีเมลของคุณ</label>
-            <input type="email" class="form-control" id="emailInput" placeholder="example@email.com" required>
-          </div>
-          <button type="submit" class="btn btn-primary w-100">ส่งคำขอรีเซ็ตรหัสผ่าน</button>
-        </form>
-      </div>
-    </div>
-  </div>
-</div> -->
 </section>
 
 

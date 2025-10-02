eb5d5d31dea417259d021c07f2bf5f017d5e5bad
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-10-02 20:47:49 +0700

    edit font v.3

--- app/Views/templates/Pc/header-login.php
+++ app/Views/templates/Pc/header-login.php
@@ -3,9 +3,7 @@
     <title>PC Detail</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Sarabun&display=swap" rel="stylesheet">
-
+   
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>

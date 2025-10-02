daaeaa86e4ceb735f98d84b361a1ab4f3918e325
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-10-02 20:34:17 +0700

    add css

--- app/Views/templates/Pc/header-login.php
+++ app/Views/templates/Pc/header-login.php
@@ -9,6 +9,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
 
 <link rel="stylesheet" href="css/login.css">

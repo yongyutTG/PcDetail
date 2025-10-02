aab871cb4945a8a1f03d2ae14b3158aac2253870
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-10-02 22:15:57 +0700

    edit

--- app/Views/templates/Pc/header-login.php
+++ app/Views/templates/Pc/header-login.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <title>PC Detail</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> -->
 

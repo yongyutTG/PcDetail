fd6683b599112eb288de9c1f93ea1240e067d7b4
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-10-02 20:45:52 +0700

    edit font

--- app/Views/pages/Pc/auth/login.php
+++ app/Views/pages/Pc/auth/login.php
@@ -1,6 +1,5 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
 <section>

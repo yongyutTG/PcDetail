a0d3dbce19925a43a6dabe0f6bd5c3fe57b6852d
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-10-04 01:18:33 +0700

    edit

--- app/Controllers/Pc/AuthPc.php
+++ app/Controllers/Pc/AuthPc.php
@@ -113,7 +113,7 @@ class AuthPc extends BaseController
 
          // ส่งอีเมลด้วย Email Library ของ CI4
         $email = \Config\Services::email();
-        $email->setFrom('info@tgsaving.com', 'ระบบรีเซ็ตรหัสผ่าน');
+        $email->setFrom('yongyuttgsaving@gmail.com', 'ระบบรีเซ็ตรหัสผ่าน');
         $email->setTo($to);
         $email->setSubject($subject);
         $email->setMessage($message);

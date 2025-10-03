216b68bc454bfe598e4556d17b27d9ed1e5299bd
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-10-04 01:39:02 +0700

    edit

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -10,112 +10,17 @@ class Email extends BaseConfig
     public string $fromName   = 'TG Saving System';
     public string $recipients = '';
 
-    /**
-     * The "user agent"
-     */
-    public string $userAgent = 'CodeIgniter';
+    public string $protocol   = 'smtp';
+    public string $SMTPHost   = 'smtp.tgsaving.com';
+    public string $SMTPUser   = 'info@tgsaving.com';
+    public string $SMTPPass   = 'infoadmin028717';
+    public int    $SMTPPort   = 587; // หรือ 465 ถ้าใช้ SSL
+    public string $SMTPCrypto = 'tls'; // หรือ 'ssl'
 
-    /**
-     * The mail sending protocol: mail, sendmail, smtp
-     */
-    public string $protocol = 'smtp';
-
-    /**
-     * The server path to Sendmail.
-     */
-    public string $mailPath = '/usr/sbin/sendmail';
-
-    /**
-     * SMTP Server Hostname
-     */
-    public string $SMTPHost = 'smtp.tgsaving.com';
-
-    /**
-     * SMTP Username
-     */
-    public string $SMTPUser = 'info@tgsaving.com';
-
-    /**
-     * SMTP Password
-     */
-    public string $SMTPPass = 'infoadmin028717';
-
-    /**
-     * SMTP Port
-     */
-    public int $SMTPPort = 587;
-
-    /**
-     * SMTP Timeout (in seconds)
-     */
-    public int $SMTPTimeout = 20;
-
-    /**
-     * Enable persistent SMTP connections
-     */
-    public bool $SMTPKeepAlive = false;
-
-    /**
-     * SMTP Encryption.
-     *
-     * @var string '', 'tls' or 'ssl'. 'tls' will issue a STARTTLS command
-     *             to the server. 'ssl' means implicit SSL. Connection on port
-     *             465 should set this to ''.
-     */
-    public string $SMTPCrypto = 'tls';
-
-    /**
-     * Enable word-wrap
-     */
-    public bool $wordWrap = true;
-
-    /**
-     * Character count to wrap at
-     */
-    public int $wrapChars = 76;
-
-    /**
-     * Type of mail, either 'text' or 'html'
-     */
-    public string $mailType = 'html';
-
-    /**
-     * Character set (utf-8, iso-8859-1, etc.)
-     */
-    public string $charset = 'UTF-8';
-
-    /**
-     * Whether to validate the email address
-     */
-    public bool $validate = false;
-
-    /**
-     * Email Priority. 1 = highest. 5 = lowest. 3 = normal
-     */
-    public int $priority = 3;
-
-    /**
-     * Newline character. (Use “\r\n” to comply with RFC 822)
-     */
-    public string $CRLF = "\r\n";
-
-    /**
-     * Newline character. (Use “\r\n” to comply with RFC 822)
-     */
-    public string $newline = "\r\n";
-
-    /**
-     * Enable BCC Batch Mode.
-     */
-    public bool $BCCBatchMode = false;
-
-    /**
-     * Number of emails in each BCC batch
-     */
-    public int $BCCBatchSize = 200;
-
-    /**
-     * Enable notify message from server
-     */
-    public bool $DSN = false;
+    public int    $SMTPTimeout = 30;
+    public string $mailType    = 'html';
+    public string $charset     = 'utf-8';
+    public string $newline     = "\r\n";
+    public string $CRLF        = "\r\n";
 }
+

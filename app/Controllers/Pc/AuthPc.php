55fa86e3d02e5563fcc15e6064a2370f5cdcd502
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-10-05 17:53:28 +0700

    edit

--- app/Controllers/Pc/AuthPc.php
+++ app/Controllers/Pc/AuthPc.php
@@ -94,8 +94,8 @@ class AuthPc extends BaseController
 
 
          //กรณีไรับค่ารหัสผ่านใหม่จาก user
-       //$newPassword_gen = $this->request->getPost('new_password');
-        $newPassword_gen = substr(str_shuffle('abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789'), 0, 8);
+       $newPassword_gen = $this->request->getPost('new_password');
+       // $newPassword_gen = substr(str_shuffle('abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789'), 0, 8);
         //$newPassword = substr(md5(uniqid(rand(), true)), 0, 8); // รหัสผ่านใหม่ 8 ตัวอักษร
        
        

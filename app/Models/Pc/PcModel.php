3ca1212841a3bbb74764408650941412b4c6231d
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-10-10 21:45:58 +0700

    edit

--- app/Models/Pc/PcModel.php
+++ app/Models/Pc/PcModel.php
@@ -13,7 +13,6 @@ public function getAllDetails($limit =17, int $offset = 0)
 {
 $sql = "SELECT * 
         FROM pc_detail_master 
-        where use_status= 'A'
         ORDER BY pc_id
         OFFSET ? ROWS FETCH NEXT ? ROWS ONLY";
 return $this->db->query($sql, [$offset, $limit])->getResultArray();

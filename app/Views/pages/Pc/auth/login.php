e6613d59372e8f579e493c165182fed2b0965e73
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-10-10 22:40:50 +0700

    edit

--- app/Views/pages/Pc/auth/login.php
+++ app/Views/pages/Pc/auth/login.php
@@ -2,6 +2,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/blueimp-md5/2.19.0/js/md5.min.js"></script>
 

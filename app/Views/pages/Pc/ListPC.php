8cb52aa5bdc693358e10f86e03cb699ed8c226e7
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-10-15 00:00:41 +0700

    edit

--- app/Views/pages/Pc/ListPC.php
+++ app/Views/pages/Pc/ListPC.php
@@ -10,7 +10,7 @@
           <div class="card shadow-sm">
             <div class="card-body">
               <div class="d-flex align-items-center gap-2 mb-3">
-                <input type="text" id="keywordSearch" class="form-control" style="width:400px;" placeholder="ค้นหา...">
+                <input type="text" id="keywordSearch" style="width:400px;" placeholder="ค้นหา...">
                 <br>
                 <label for="typeFilter" class="mb-0 fw-bold">ประเภทอุปกรณ์: </label>
                 <select id="typeFilter"
@@ -1135,7 +1135,7 @@ function afterRenderTable() {
     if (searchInput) {
       searchInput.focus();
     }
-    
+
  document.addEventListener("click", function (e) {
   const btn = e.target.closest(".copy-ip-btn");
   if (!btn) return;

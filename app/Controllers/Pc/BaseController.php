7e6262ba527bd0c63a89cac62360fcd6ff9cb10f
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2025-10-20 22:44:27 +0700

    edit

--- app/Controllers/Pc/BaseController.php
+++ app/Controllers/Pc/BaseController.php
@@ -53,7 +53,9 @@ abstract class BaseController extends Controller
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
         // Do Not Edit This Line
-        //parent::initController($request, $response, $logger);
+        parent::initController($request, $response, $logger);
+
+        // Preload any models, libraries, etc, here.
   // โหลด session ถ้ายังไม่ได้เปิด
     $this->session = \Config\Services::session();
 
@@ -73,8 +75,6 @@ abstract class BaseController extends Controller
     // ✅ แชร์ตัวแปรไปยังทุก view
     view()->setVar('dbName', $this->dbName);
     view()->setVar('dbDate', $this->dbDate);
-
-        // Preload any models, libraries, etc, here.
 
         // E.g.: $this->session = \Config\Services::session();
     }

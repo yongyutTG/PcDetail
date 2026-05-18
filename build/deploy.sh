c5097d7694220368e25f739c6f85f3d76f0f6882
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2026-05-18 22:18:26 +0700

    edit start depoly

--- build/deploy.sh
+++ build/deploy.sh
@@ -7,13 +7,6 @@ cd /var/www/html/PcDetail
 echo "Pull ล่าสุดจาก GitHub"
 git pull origin main
 
-echo "Install Composer"
-composer install --no-dev --optimize-autoloader
-
-
-echo "Permission"
-chmod -R 775 writable
-
 echo "Reload NGINX"
 sudo systemctl reload nginx
 

0000d2e10e9bc9bc6563c9fa84ec18cee48714e0
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2026-05-18 21:47:50 +0700

    add deploy

---
+++ build/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "===== START DEPLOY ====="
+
+cd /var/www/html/ci4-project
+
+echo "Pull ล่าสุดจาก GitHub"
+git pull origin main
+
+echo "Install Composer"
+composer install --no-dev --optimize-autoloader
+
+
+echo "Permission"
+chmod -R 775 writable
+
+echo "Reload NGINX"
+sudo systemctl reload nginx
+
+echo "===== DEPLOY SUCCESS ====="

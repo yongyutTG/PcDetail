72273d51c6bda7b433a45d78b010cf3c71b67490
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2026-05-19 11:54:56 +0700

    edit app

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
     // public string $baseURL = 'http://localhost:8080';
-public string $baseURL = 'http://172.17.1.105/PcDetail/';
+public string $baseURL = 'http://172.17.1.106/PcDetail/';
 
 
     /**

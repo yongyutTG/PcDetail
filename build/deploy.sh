6d884ec4aa3d6d3609899eef4a24fd739a87d00c
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2026-05-19 10:20:57 +0700

    edit deploy

--- build/deploy.sh
+++ build/deploy.sh
@@ -7,7 +7,7 @@ cd /var/www/html/PcDetail
 echo "Pull ล่าสุดจาก GitHub"
 git pull origin main
 
-echo "Reload NGINX"
-sudo systemctl reload nginx
+# echo "Reload NGINX"
+# sudo systemctl reload nginx
 
 echo "===== DEPLOY SUCCESS ====="

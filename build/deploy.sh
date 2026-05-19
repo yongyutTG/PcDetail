7d07bf37ed837102c56d68abd7f79163f19adaa4
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2026-05-19 11:49:35 +0700

    Refactor code structure for improved readability and maintainability

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 echo "===== START DEPLOY ====="
-
 cd /var/www/html/PcDetail
 
 echo "Pull ล่าสุดจาก GitHub"
 git pull origin main
+
+echo "===== Composer Install ====="
+composer install --no-dev --optimize-autoloader
 
 # echo "Reload NGINX"
 # sudo systemctl reload nginx

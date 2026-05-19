bf7c2c9da4d316c8bc2ad4c51210309721eab1ac
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2026-05-19 21:28:13 +0700

    edit

--- app/Views/pages/Pc/ListPC.php
+++ app/Views/pages/Pc/ListPC.php
@@ -34,9 +34,9 @@
                 <label for="statusFilter" class="mb-0 fw-bold">สถานะ: </label>
                 <select id="statusFilter"
                   style="padding:6px; font-size: 14px; font-weight: bold; border-radius:6px; border:1px solid #ccc;">
-                  <!-- <option value="">ทั้งหมด</option> -->
                   <option value="A">ใช้งาน</option>
                   <option value="N">ไม่ใช้งาน</option>
+                   <option value="">ทั้งหมด</option>
                 </select>
                 <br>
                 <button type="button" id="resetBtn" class="btn btn-reset">
@@ -694,18 +694,11 @@
     resetBtn.addEventListener("click", () => {
       searchInput.value = "";
       statusFilter.value = "A";
+      // statusFilter.value = "";
       brnoFilter.value = "";
       typeFilter.value = "";
       fetchPCs({ page: 1 });
     });
-
-
- // --- assume these are already defined in your code ---
-//const tbody = document.querySelector("tbody"); // ปรับ selector ตามจริง
-//const searchInput = document.getElementById("searchInput"); // ตัวอย่าง
-// statusFilter, brnoFilter, typeFilter ตามที่คุณใช้
-// ---------------------------------------------------
-
 
 
 function highlightRow(index) {

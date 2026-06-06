fb3d193f50a9c479b7421290f0672e36846f710d
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2026-06-06 23:47:05 +0700

    Update deploy script and add launch configuration for debugging

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+set -e
 
 echo "===== START DEPLOY ====="
 cd /var/www/html/PcDetail
 
-echo "Pull ล่าสุดจาก GitHub"
-git pull origin main
+echo "Code ล่าสุดจาก GitHub"
+#git pull origin main;  #ดึงข้อมูลล่าสุดจาก remote repository และ merge เข้ากับ branch ปัจจุบัน
+git fetch origin  #ดึงข้อมูลล่าสุดจาก remote repository
+git reset --hard origin/main    #รีเซ็ต branch ปัจจุบันให้ตรงกับ origin/main
+git clean -fd  #ใช้ลบไฟล์ที่ Git ไม่รู้จัก
 
 echo "===== Composer Install ====="
-composer install --no-dev --optimize-autoloader
+composer install --no-dev --optimize-autoloader --no-interaction
 
-# echo "Reload NGINX"
-# sudo systemctl reload nginx
+echo "Reload NGINX"
+sudo systemctl reload nginx
+
 
 echo "===== DEPLOY SUCCESS ====="

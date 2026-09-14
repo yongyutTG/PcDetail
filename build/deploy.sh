6bd51b241eb2b02f95ee21578550ac74baab34ea
yongyut.wacharapongmatee <yongyuttgsaving@gmail.com>
2026-09-14 23:55:39 +0700

    refactor: remove composer installation steps from deploy script

--- build/deploy.sh
+++ build/deploy.sh
@@ -8,17 +8,6 @@ cd /var/www/html/PcDetail
 echo "Code ล่าสุดจาก GitHub"
 git fetch origin
 git reset --hard origin/main
-git clean -fd -e composer.phar -e .env
-
-echo "===== Composer Install ====="
-if [ ! -f composer.phar ]; then
-  echo "Download Composer 2.8.6"
-  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-  php composer-setup.php --version=2.8.6
-  rm composer-setup.php
-fi
-
-php composer.phar install --no-dev --optimize-autoloader --no-interaction
 
 echo "Reload PHP-FPM and NGINX"
 sudo systemctl restart php7.4-fpm
